3b08b71d9eea4ffc529551d8fcce1dd8c285b071
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-12-22 00:08:35 +0300

    convert list of maps to list of objects

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,11 +1,11 @@
 class ArticleModel {
-  final String image;
+  final String? image;
   final String title;
-  final String subTitle;
+  final String? description;
 
   ArticleModel({
     required this.image,
     required this.title,
-    required this.subTitle,
+    required this.description,
   });
 }
--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:news_app/models/article_model.dart';
 
 class NewsService {
   final Dio dio;
@@ -11,5 +12,17 @@ class NewsService {
     Map<String, dynamic> jsonData = response.data;
 
     List<dynamic> articles = jsonData["articles"];
+
+    List<ArticleModel> articlesList = [];
+
+    for (var article in articles) {
+      ArticleModel articleModel = ArticleModel(
+        image: article["urlToImage"],
+        title: article["title"],
+        description: article["description"],
+      );
+      articlesList.add(articleModel);
+    }
+    print(articlesList);
   }
 }
